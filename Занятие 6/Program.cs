cf18fd349d7c1269c99d828b3668f9fbdebeef72
s.fedorovich <s.fedorovich@CZC71776GT>
2021-10-28 10:12:54 +0300

    Задача 1

--- Занятие 6/Program.cs
+++ Занятие 6/Program.cs
@@ -9,7 +9,23 @@ namespace Занятие_6
     class Program
     {
         static void Main(string[] args)
+            // нахождение самого длинного слова в предложении
         {
+            Console.WriteLine("Введите предложение:");
+            string s = Console.ReadLine();
+            string[] array = s.Split(new Char[] { ' ', '.', ',', ';', ':', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+            int index = 0;
+            int max = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i].Length>index)
+                {
+                    index = array[i].Length;
+                    max = i;
+                }
+            }
+            Console.WriteLine("Самое длинное слово в предложении: {0}", array[max]);
+            Console.ReadKey();
         }
     }
 }
