29ebca262447a84611a2efab548ef0aa459cbe70
s.fedorovich <s.fedorovich@CZC71776GT>
2021-10-28 10:43:39 +0300

    Задача 1 и задача 2

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -9,7 +9,27 @@ namespace Задача_2
     class Program
     {
         static void Main(string[] args)
-        {
+        { // являлется ли строка палиндромом
+            {
+                Console.WriteLine("Введите предложение без знаков препинания:");
+                string s = Console.ReadLine().Replace(" ", "").ToLower();
+                string t = s;
+                string reverse = "";
+                for (int i = t.Length - 1; i >= 0; i--)
+                {
+                    reverse += t[i];
+                }
+                if (t == reverse)
+                {
+                    Console.WriteLine("Вы ввели палиндром!");
+                }
+                else
+                {
+                    Console.WriteLine("Предложение не является палиндромом!");
+                }
+                Console.ReadKey();
+            }
         }
     }
 }
+   
